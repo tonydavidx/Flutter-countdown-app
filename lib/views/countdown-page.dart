35d6caefc49a105fa8d3d51c3d1a63ec989da50e
Tony David <tonydavidx@gmail.com>
2021-08-08 00:16:42 +0530

    Project Start

---
+++ lib/views/countdown-page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import '../widgets/round-button.dart';
+
+class CountdownPage extends StatefulWidget {
+  const CountdownPage({Key? key}) : super(key: key);
+
+  @override
+  _CountdownPageState createState() => _CountdownPageState();
+}
+
+class _CountdownPageState extends State<CountdownPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xfff5fbff),
+      body: Column(
+        children: [
+          Expanded(
+            child: Center(
+              child: GestureDetector(
+                child: Text(
+                  '1:24:20',
+                  style: TextStyle(
+                    fontSize: 60,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                RoundButton(
+                  icon: Icons.play_arrow,
+                ),
+                RoundButton(
+                  icon: Icons.stop,
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
