cb251ddd6beab8d024796b486b39e632536ae23c
Tony David <tonydavidx@gmail.com>
2021-08-10 17:07:11 +0530

    countdown timer completed

--- lib/views/countdown-page.dart
+++ lib/views/countdown-page.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../widgets/round-button.dart';
+import 'package:flutter_ringtone_player/flutter_ringtone_player.dart';
 
 class CountdownPage extends StatefulWidget {
   const CountdownPage({Key? key}) : super(key: key);
@@ -8,7 +10,56 @@ class CountdownPage extends StatefulWidget {
   _CountdownPageState createState() => _CountdownPageState();
 }
 
-class _CountdownPageState extends State<CountdownPage> {
+class _CountdownPageState extends State<CountdownPage>
+    with TickerProviderStateMixin {
+  late AnimationController controller;
+
+  bool isPlaying = false;
+
+  String get countText {
+    Duration count = controller.duration! * controller.value;
+    return controller.isDismissed
+        ? '${controller.duration!.inHours}:${(controller.duration!.inMinutes % 60).toString().padLeft(2, '0')}:${(controller.duration!.inSeconds % 60).toString().padLeft(2, '0')}'
+        : '${count.inHours}:${(count.inMinutes % 60).toString().padLeft(2, '0')}:${(count.inSeconds % 60).toString().padLeft(2, '0')}';
+  }
+
+  double progress = 1.0;
+
+  void notify() {
+    if (countText == '0:00:00') {
+      FlutterRingtonePlayer.playNotification();
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 60),
+    );
+
+    controller.addListener(() {
+      notify();
+      if (controller.isAnimating) {
+        setState(() {
+          progress = controller.value;
+        });
+      } else {
+        setState(() {
+          progress = 1.0;
+          isPlaying = false;
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,14 +67,49 @@ class _CountdownPageState extends State<CountdownPage> {
       body: Column(
         children: [
           Expanded(
-            child: Center(
-              child: Text(
-                '1:24:20',
-                style: TextStyle(
-                  fontSize: 60,
-                  fontWeight: FontWeight.bold,
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                SizedBox(
+                  width: 300,
+                  height: 300,
+                  child: CircularProgressIndicator(
+                    backgroundColor: Colors.grey.shade300,
+                    value: progress,
+                    strokeWidth: 6,
+                  ),
                 ),
-              ),
+                GestureDetector(
+                  onTap: () {
+                    if (controller.isDismissed) {
+                      showModalBottomSheet(
+                        context: context,
+                        builder: (context) => Container(
+                          height: 300,
+                          child: CupertinoTimerPicker(
+                            initialTimerDuration: controller.duration!,
+                            onTimerDurationChanged: (time) {
+                              setState(() {
+                                controller.duration = time;
+                              });
+                            },
+                          ),
+                        ),
+                      );
+                    }
+                  },
+                  child: AnimatedBuilder(
+                    animation: controller,
+                    builder: (context, child) => Text(
+                      countText,
+                      style: TextStyle(
+                        fontSize: 60,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
           Padding(
@@ -31,11 +117,35 @@ class _CountdownPageState extends State<CountdownPage> {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                RoundButton(
-                  icon: Icons.play_arrow,
+                GestureDetector(
+                  onTap: () {
+                    if (controller.isAnimating) {
+                      controller.stop();
+                      setState(() {
+                        isPlaying = false;
+                      });
+                    } else {
+                      controller.reverse(
+                          from: controller.value == 0 ? 1.0 : controller.value);
+                      setState(() {
+                        isPlaying = true;
+                      });
+                    }
+                  },
+                  child: RoundButton(
+                    icon: isPlaying == true ? Icons.pause : Icons.play_arrow,
+                  ),
                 ),
-                RoundButton(
-                  icon: Icons.stop,
+                GestureDetector(
+                  onTap: () {
+                    controller.reset();
+                    setState(() {
+                      isPlaying = false;
+                    });
+                  },
+                  child: RoundButton(
+                    icon: Icons.stop,
+                  ),
                 ),
               ],
             ),
